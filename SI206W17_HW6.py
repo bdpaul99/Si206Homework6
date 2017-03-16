5ae9cf6eb6806f9c6ec7eee8c209b244c10d33fb
bdpaul99 <bddpaul@me.com>
2017-03-15 21:34:31 -0400

    finished the assignment

--- SI206W17_HW6.py
+++ SI206W17_HW6.py
@@ -31,9 +31,10 @@ class Student():
         return this_Student.years_UM
 
     # Define the additional method here
-    def write_programs(int_to_add=1):
+    def write_programs(self, int_to_add=1):
         self.num_programs  = self.num_programs + int_to_add
-    
+    def get_productivity(self):
+        return (self.num_programs / self.years_UM)
 
 #### DONE WITH STUDENT CLASS DEFINITION
 
@@ -99,7 +100,7 @@ student_tups_list = list(student_tups)
 ## [PROBLEM 5]
 print("\n\n***** Problem 5 *****")
 # Use a list comprehension to create a list of Student instances out of the student_tups list you just created in Problem 2, and save that list in a variable called programmers. You should make sure you pass these tests before continuing, as you'll need this list for problems later on!
-
+programmers = [Student(tup[0],tup[1],tup[2]) for tup in student_tups_list]
 
 
 ## [PROBLEM 6]
@@ -108,17 +109,16 @@ print("\n\n***** Problem 6 *****")
 # A Student's programming_productivity is defined as that student's number of programs written divided by the years they have been at UMich.
 
 # Use the Python map function on the programmers list you just created, in order to create an map instance iterator of numbers representing the productivity of each student. Save the map iterator in a variable called prod_iter.
-
+prod_iter = map(lambda x: (x.get_productivity()),programmers)
 ## Write code to cast that iterator to a list. Save that list in the variable prod_list.
-
+prod_list = list(prod_iter)
 ## You may add a method to the Student class if you wish in order to do this, but you do not need to. (If you do, make sure you do not create any syntax errors that keep code/tests from running!)
-
 
 
 ## [PROBLEM 7]
 print("\n\n***** Problem 7 *****")
 # Create a list of tuples wherein each tuple has a student's name and productivity value. Save the list of tuples in a variable called names_and_productivities. To do this, you should use a list comprehension (you may also use the zip function, and you may use any variables you have already created).
-
+names_and_productivities = [(x[0],x[1]) for x in zip(names,prod_list)]
 ## But be careful that if you use answers from previous problems, you use the LISTs you generated, so that all your tests can still pass and you avoid confusion!
 
 
@@ -126,17 +126,20 @@ print("\n\n***** Problem 7 *****")
 ## [PROBLEM 8]
 print("\n\n***** Problem 8 *****")
 # Use the Python filter function to select the subset of programmers who have names with 5 or more characters. (i.e. ["Albert","Dinesh","Euijin"]) Your result should be an filter object that points to Student instances. Save that filter iterator in a variable called long_names.
-
+long_names = filter(lambda x: len(x.name) >= 5, programmers)
 
 
 ## Then write code to cast the value of long_names to a list and save it in the variable long_names_list. 
 
-
+long_names_list = list(long_names)
 
 ## [PROBLEM 9]
 print("\n\n***** Problem 9 *****")
 
 # Use a list comprehension to generate a LIST of just the names of those Student instances whose name is longer than their seniority (i.e., ["Albert", "Mai", "Dinesh", "Euijin"]). Assign it to a variable called names_with_not_too_much_seniority.
+
+names_with_not_too_much_seniority = [x.name for x in programmers if len(x.name) > x.years_UM]
+
 
 ## Note that you can use another list you have already created for this problem.
 
@@ -159,20 +162,27 @@ print("\n\n***** Problem 10 *****")
 ## We have provided files samplehw6_1.txt and samplehw6_2.txt for your use for this problem, which hopefully you have downloaded, so you can test with those file names! The test below also relies upon these files. Of course, you could also create other files for testing.
 
 # Define readfiles (make sure to close the file reference in the right place)
-
+def readfiles(file_names):
+    for f in file_names:
+        fileref = open(f,'r')
+        for line in fileref: 
+            yield line
+        fileref.close() 
 
 # Define len_check
-
-
+def len_check(lines):
+    return (line for line in lines if len(line) > 40)
 # Define main_filterer
+def main_filterer(filenames):
+    lines  = readfiles(filenames)
+    return len_check(lines)
 
 
-
-## Uncomment this code to test so you can see easily what results from your code. DO uncomment it. DO NOT delete or change it. (You can add other code above while you work, of course.)
+# # Uncomment this code to test so you can see easily what results from your code. DO uncomment it. DO NOT delete or change it. (You can add other code above while you work, of course.)
 # provided_file_names = ["samplehw6_1.txt","samplehw6_2.txt"]
 # for ln in main_filterer(provided_file_names):
 #     print(ln.rstrip('\n'), end=" ")
-#####
+# ####
 
 
 ##### TESTS BELOW THIS LINE. DO NOT CHANGE ANY CODE BELOW THIS LINE. #####
